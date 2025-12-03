2c5740163059f2d6bdf8bbaeb6feeeb97aff9ca7
Aitana <aitana.buiza@gracia.lasalle.cat>
2025-12-03 16:23:09 +0100

    Add melody

--- main.ts
+++ main.ts
@@ -9,7 +9,14 @@ basic.forever(function () {
 basic.forever(function () {
     basic.showNumber(input.lightLevel())
     if (input.lightLevel() > 200) {
-    	
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("G B G E G C5 G A ", 450), music.PlaybackMode.UntilDone)
     } else if (input.lightLevel() < 50) {
     	
     } else {
