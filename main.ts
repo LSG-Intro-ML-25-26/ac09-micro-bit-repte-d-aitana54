11e2c2f6c89fffa4a21115beff8c8b71b45c6161
Aitana <aitana.buiza@gracia.lasalle.cat>
2025-12-03 15:56:45 +0100

    Add conditional

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 basic.forever(function () {
-    basic.showNumber(input.lightLevel())
-})
-basic.forever(function () {
     basic.showNumber(input.temperature())
     if (input.temperature() > 22) {
         basic.showString("Heat")
     } else {
         basic.showString("Cold")
+    }
+})
+basic.forever(function () {
+    basic.showNumber(input.lightLevel())
+    if (input.lightLevel() > 200) {
+    	
+    } else {
+    	
     }
 })
